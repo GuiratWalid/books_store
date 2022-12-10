c2e5f165e0f82ee1a5a5d502bcf938023a95aa54
unknown <guiratwalid98@gmail.com>
2022-12-10 13:25:10 +0100

    Books Pages

---
+++ books_store/Data/books_storeContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using books_store.Models;
+
+namespace books_store.Data
+{
+    public class books_storeContext : DbContext
+    {
+        public books_storeContext (DbContextOptions<books_storeContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<books_store.Models.Book> Book { get; set; } = default!;
+    }
+}
--- books_store/Program.cs
+++ books_store/Program.cs
@@ -1,4 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using books_store.Data;
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<books_storeContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("books_storeContext") ?? throw new InvalidOperationException("Connection string 'books_storeContext' not found.")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
